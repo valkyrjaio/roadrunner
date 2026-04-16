15d0ab1130675e2a55e4748188cda4fc364a59f9
Melech Mizrachi <melechmizrachi@gmail.com>
2026-04-16 14:53:58 -0700

    [Project] Add workflows, add missing documentation, update composer (#4)
    # Description
    
    Add workflows, add missing documentation, update composer.
    
    ## Types of changes
    
    - [X] Improvement _(non-breaking change which improves code)_
        <!-- Target the lowest major affected branch -->
    - [ ] Bug fix _(non-breaking change which fixes an issue)_
        <!-- Target the lowest major affected branch -->
    - [ ] New feature _(non-breaking change which adds functionality)_
        <!-- Target master -->
    - [ ] Deprecation _(breaking change which removes functionality)_
        <!-- Target master -->
    - [ ] Breaking change _(fix or feature that would cause existing
    functionality
      to change)_
    <!-- Target master, unless this is a bug fix in which case let's chat
    -->
    - [ ] Documentation improvement
        <!-- Target appropriate branch -->

---
+++ tests/Tests/Abstract/RoadRunnerTestCase.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Valkyrja RoadRunner package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Valkyrja\RoadRunner\Tests\Abstract;
+
+use Valkyrja\PhpUnit\Abstract\ValkyrjaTestCase;
+
+/**
+ * Test case for tests.
+ */
+abstract class RoadRunnerTestCase extends ValkyrjaTestCase
+{
+}
